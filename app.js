86ef61bcbd8aa0e1ac26032d660228971978c042
9insane9 <145199591+9insane9@users.noreply.github.com>
2025-09-07 18:27:42 +0300

    intial commit, make app

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require("node:path")
+const express = require("express")
+const { indexRouter } = require("./routes/indexRouter")
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", indexRouter)
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public", "404.html"))
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send("500: Server Error")
+})
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error
+  }
+  console.log(`Message board listening on port ${PORT}!`)
+})
