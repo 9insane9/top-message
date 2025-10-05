e53681381eec9835893387ce77e63e832cba5f31
9insane9 <145199591+9insane9@users.noreply.github.com>
2025-10-05 19:54:38 +0300

    remove logs

--- db/pool.js
+++ db/pool.js
@@ -5,9 +5,6 @@ const connectionString = process.env.NEON_CONNECTION_STRING.replace(
   ""
 )
 
-console.log("DATABASE_URL raw:", process.env.NEON_CONNECTION_STRING)
-console.log("Length:", process.env.NEON_CONNECTION_STRING?.length)
-
 module.exports = new Pool({
   connectionString,
 })
