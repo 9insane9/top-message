c3cc12ff5a28f053195ebe79ec7e83a4613010a1
9insane9 <145199591+9insane9@users.noreply.github.com>
2025-10-05 20:05:51 +0300

    ...

--- db/pool.js
+++ db/pool.js
@@ -1,24 +1,24 @@
 const { Pool } = require("pg")
 
-const connectionString = process.env.NEON_CONNECTION_STRING.replace(
+const connectionString = process.env.NEON_CONNECTION_STRING?.replace(
   /^"+|"+$/g,
   ""
 )
 
-module.exports = new Pool({
+const pool = new Pool({
   connectionString,
   ssl: { rejectUnauthorized: false },
 })
 
-async function test() {
+async function testConnection() {
   try {
     const res = await pool.query("SELECT NOW()")
     console.log("Connected!", res.rows[0])
   } catch (err) {
     console.error("Connection failed:", err)
-  } finally {
-    await pool.end()
   }
 }
 
-test()
+testConnection()
+
+module.exports = pool
